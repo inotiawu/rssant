331f32a93e52afabe674609474744c69a0996df9
inotiawu <ghyy1038@126.com>
2023-01-01 14:25:33 +0800

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+docker volume create rssant-data || true
+docker volume create rssant-postgres-data || true
+docker volume create rssant-postgres-logs || true
+
+docker rm -f rssant || true
+docker run -ti --name rssant -d \
+    -p 6789:80 \
+    --env-file ~/rssant/rssant.env \
+    -v rssant-data:/app/data \
+    -v rssant-postgres-data:/var/lib/postgresql/11/main \
+    -v rssant-postgres-logs:/var/log/postgresql \
+    --log-driver json-file --log-opt max-size=50m --log-opt max-file=10 \
+    --restart unless-stopped \
+    guyskk/rssant:latest
+
+docker logs --tail 1000 -f rssant
